f289f9ad95a301dfe4f240e17534d316bc078345
Luiz Fernando <luiz.fernando_1997@hotmail.com>
2018-10-09 22:44:24 -0300

    organizando pastas

--- Java/exceções/ByteBank-Biblioteca/src/teste.java
+++ Java/exceções/ByteBank-Biblioteca/src/teste.java
@@ -1,7 +1,10 @@
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
 
 public class teste {
     public static void main(String[] args) {
         System.out.println("Teste");
-        Conta
+        Conta c = new ContaCorrente(132132,123132);
+
     }
 }
