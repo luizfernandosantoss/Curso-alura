60f1430ea33d79c135e93204a083cd819f1eed0c
Luiz Fernando <luiz.fernando_1997@hotmail.com>
2018-10-12 09:52:32 -0300

    Java parte 6: Conhecendo o java.util

--- Java/exceções/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ Java/exceções/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,7 +1,6 @@
 package br.com.bytebank.banco.teste;
 
 
-import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
