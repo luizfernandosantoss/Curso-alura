61bdd9db86beb11edc15d23e44183e6caa73fc70
Luiz Fernando <luiz.fernando_1997@hotmail.com>
2018-10-13 00:56:25 -0300

    Criando um array de Contas

--- Java/exceções/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ Java/exceções/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,7 +1,27 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
 public class Teste {
+    //Array
     public static void main(String[] args) {
-        int
+
+        int[] idades = new int[5];
+
+        for (int i = 0; i < idades.length ; i++) {
+            idades[i] = i*i;
+            System.out.println(idades[i]);
+        }
+        ContaCorrente[] contas = new ContaCorrente[5];
+        ContaCorrente cc1 = new ContaCorrente(9178,06235-3);
+        ContaCorrente cc2 = new ContaCorrente(9178,062544-3);
+
+        contas[0] = cc1;
+        contas[1] = cc2;
+
+        System.out.println(cc1.getAgencia());
+        System.out.println(contas[0].getAgencia());
+
     }
 }
