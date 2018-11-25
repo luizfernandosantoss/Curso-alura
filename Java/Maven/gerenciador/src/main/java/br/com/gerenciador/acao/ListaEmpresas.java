9813f915e7bb646ba58987dbdd706b80df3712b0
Luiz Fernando <luiz.fernando_1997@hotmail.com>
2018-11-25 15:04:10 -0200

    atualizando o projeto ultilizando o Json

--- Java/Maven/gerenciador/src/main/java/br/com/gerenciador/acao/ListaEmpresas.java
+++ Java/Maven/gerenciador/src/main/java/br/com/gerenciador/acao/ListaEmpresas.java
@@ -1,18 +1,14 @@
 package br.com.gerenciador.acao;
 
-import br.com.gerenciador.modelo.Banco;
-import br.com.gerenciador.modelo.Empresa;
-import org.hibernate.Session;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+import br.com.gerenciador.modelo.Banco;
+import br.com.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas extends HttpServlet implements Acao {
 
